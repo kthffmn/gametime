f35e5e3c784dd8eca55bde5987c7b85bb45cec99
Katie Hoffman <ktahoffman@gmail.com>
2014-04-21 11:04:28 -0400

    adds angular to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  # angular
+  get '/games-angular', :to => redirect('/app/index.html#/games')
+  get '/404-angular', :to => redirect('app/index.html#/404')
+  
+  # games
   resources :games
+
 end
