b1275605a50019e4df121a59b4571120d9437050
Katie Hoffman <ktahoffman@gmail.com>
2014-04-22 11:18:35 -0400

    adds games/index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  # root/home
+  root 'games#index'
+
   # angular
   get '/games-angular', :to => redirect('/app/index.html#/games')
   get '/404-angular', :to => redirect('app/index.html#/404')
