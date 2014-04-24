b9ca0364e97b2b4652cef432668c46ed74a63407
Katie Hoffman <ktahoffman@gmail.com>
2014-04-24 10:40:04 -0400

    adds seeds for tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# make tags
+TAGS = ["accepting", "accepting", "advancing", "agreement", "association", "audience participation", "audience suggestion", "audience warm-up", "backline", "blocking", "blue", "breaking the routine", "bulldozing", "canceling", "characters", "commenting", "concentration", "conflict", "continuation", "denial", "die", "edit", "embodying", "endowing", "endowment", "energy", "environment", "exercise", "experts", "finding the game", "focus", "format", "freezing", "fuck your fear", "gibberish", "give and take", "group", "guessing", "hedging", "heightening", "host", "icebreakers", "ignoring", "instant trouble", "jabbertalk", "joining", "justifying", "lights edit", "limitations", "line game", "long form", "long form", "look and listen", "mc", "monologue", "narration", "narrative", "object work", "object work", "offer", "opening", "performance", "pimping", "platform", "props", "questions", "raising the stakes", "reincorporating", "replay", "revolving door edit", "running gag", "setup", "shelving", "short form", "sideline", "singsong", "solo", "space work", "spontaneity", "status", "status", "storytelling", "subtext", "tag out", "taking care of yourself", "third idea improv", "tilts", "time dash", "timed", "transaction scene", "transitions", "trust", "truthfulness", "verbal wit", "waffling", "walk-through", "warm-up", "wimping", "yes and"]
+TAGS.each{|tag| Tag.create(:name  => tag)}
