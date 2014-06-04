04a2a6613c0c0429d7aeec0441857e696a0e3ec9
Katie Hoffman <ktahoffman@gmail.com>
2014-06-04 12:18:39 -0400

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   
   # root/home
-  root 'games#index'
+  root 'static_pages#home'
+
+  # static pages
+  get '/home',       to: 'static_pages#home'
+  get '/about',       to: 'static_pages#about'
+  get '/contact',     to: 'static_pages#contact'
 
   # angular
-  get '/games-angular', :to => redirect('/app/index.html#/games')
-  get '/404-angular', :to => redirect('app/index.html#/404')
+  get '/games-angular', to: redirect('/app/index.html#/games')
+  get '/404-angular',   to: redirect('app/index.html#/404')
   
   # games
   resources :games
