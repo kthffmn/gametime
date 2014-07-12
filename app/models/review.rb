17669b228cccdce0773d1210d214df87688a87ac
Katie Hoffman <ktahoffman@gmail.com>
2014-07-12 15:55:26 -0400

    combines two update methods to increase speed

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,12 +2,17 @@ class Review < ActiveRecord::Base
   belongs_to :game
   belongs_to :user
 
-  before_save :update_num_of_reviews
+  before_save :update_num_of_reviews_and_total_stars
 
   private
 
-    def update_num_of_reviews
-      original = self.game.num_of_reviews
-      self.game.update(:num_of_reviews => original + 1)
+    def update_num_of_reviews_and_total_stars
+      review_num = self.game.num_of_reviews
+      star_num = self.game.total_stars
+      self.game.update(
+        :num_of_reviews => review_num + 1, 
+        :total_stars => star_num + self.stars
+      )
     end
+  # end private method
 end
