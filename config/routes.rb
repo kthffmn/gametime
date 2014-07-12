2b5eff723dfa10970b57066e6d220fb422f4bea9
Katie Hoffman <ktahoffman@gmail.com>
2014-07-12 19:07:46 -0400

    update readme, changes links /home to /

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   # static pages
-  get '/home',       to: 'static_pages#home'
   get '/about',       to: 'static_pages#about'
   get '/contact',     to: 'static_pages#contact'
 
