7635a53889753cee5f76b1f5d88393bb4bd47ff4
Katie Hoffman <ktahoffman@gmail.com>
2014-07-12 15:21:22 -0400

    refactor game model into private methods

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,33 +1,6 @@
-class GameValidator < ActiveModel::Validator
-  def validate(game)
-
-    if game.minimum > game.maximum
-      game.errors[:base] << "Minimum must be less than or equal to maximum."
-    end
-
-    unless game.early_childhood  || game.elementary_school || game.adulthood ||
-           game.middle_school    || game.high_school       || game.college
-      game.errors[:base] << "Select an age group."
-    end
-    
-    if game.names.length < 1
-      game.errors[:base] << "Missing a name."
-    end
-    
-    popularity_values = game.names.collect{|name| name.popularity }
-    num_of_popularity_values = popularity_values.length
-    if num_of_popularity_values > 1 && popularity_values.include?(0)
-      return game.errors[:base] << "Please select popularity value(s)."
-    end
-    unless num_of_popularity_values == popularity_values.uniq.length
-      game.errors[:base] << "Select different popularity values."
-    end
-  end
-end
-
 class Game < ActiveRecord::Base
-  # connections in alphebetical order
 
+  # alphebetical associations
   has_many :names
   accepts_nested_attributes_for :names, allow_destroy: true
 
@@ -42,15 +15,21 @@ class Game < ActiveRecord::Base
   accepts_nested_attributes_for :tips, allow_destroy: true
 
   has_many :reviews
-  has_many :users, :through => :reviews
+  has_many :reviewer, :through => :reviews, :class_name => "User"
 
   has_many :variations
   accepts_nested_attributes_for :variations, allow_destroy: true
 
+  # alphebetical validations
   validates :description, :presence => true, :length => { :minimum => 10, :maximum => 1000, :message => "must be between 10-1000 characters"}
   validates_numericality_of [:minimum, :maximum], :greater_than_or_equal_to => 1, only_integer: true
-  validates_with GameValidator
-    
+  validate :age_group_validation, :max_not_smaller_than_min_validation, :name_validation, :popularity_validation
+
+  # ActiveRecord instructions
+
+  before_save :update_average_rating, :update_summary
+
+  # instance methods
   def sort_names_by_popularity
     self.names.sort{|a,b| b.popularity <=> a.popularity }
   end
@@ -74,12 +53,51 @@ class Game < ActiveRecord::Base
   def all_ages?
     self.early_childhood && self.elementary_school && self.middle_school && self.high_school && self.college && self.adulthood ? true : false
   end
-
-  before_save do |game|
-    game.summary = /^(.*?)[.?!]\s/.match(game.description)[1] + "..."
-    if game.num_of_reviews > 0
-      game.average_rating = game.total_stars / game.num_of_reviews
-    end
-  end
   
+  # class methods would go below here
+
+  private
+
+    # ActiveRecord methods
+    def update_average_rating
+      if self.num_of_reviews > 0
+        self.average_rating = self.total_stars / self.num_of_reviews
+      end
+    end
+
+    def update_summary
+      self.summary = /^(.*?)[.?!]\s/.match(self.description)[1] + "..."
+    end
+
+    # validation methods
+    def age_group_validation
+      unless self.early_childhood  || self.elementary_school || self.adulthood ||
+             self.middle_school    || self.high_school       || self.college
+        errors.add(:adulthood, "Select an age group.")
+      end
+    end
+
+    def max_not_smaller_than_min_validation
+      if self.minimum > self.maximum
+        errors.add(:maximum, "Minimum must be less than or equal to maximum.")
+      end
+    end
+
+    def name_validation
+      if self.names.length < 1
+         errors.add(:name_ids, "Missing a name.")
+      end
+    end
+  
+    def popularity_validation
+      popularity_values = game.names.collect{|name| name.popularity }
+      num_of_popularity_values = popularity_values.length
+      if num_of_popularity_values > 1 && popularity_values.include?(0)
+        return errors.add(:name_ids, "Please select popularity value(s).")
+      end
+      unless num_of_popularity_values == popularity_values.uniq.length
+        errors.add(:name_ids, "Select different popularity values.")
+      end
+    end
+  #end of private method
 end
