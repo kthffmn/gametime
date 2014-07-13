06d90bb6252d36ce3b367b217ff51fb72366653a
Katie Hoffman <ktahoffman@gmail.com>
2014-07-12 20:23:35 -0400

    add user oauth through FB

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   get '/about',       to: 'static_pages#about'
   get '/contact',     to: 'static_pages#contact'
 
+  # sessions
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
   # angular
   get '/games-angular', to: redirect('/app/index.html#/games')
   get '/404-angular',   to: redirect('app/index.html#/404')
