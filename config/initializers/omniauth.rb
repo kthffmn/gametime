06c86c1b61662736e522e0bbad9dbb757437a7c5
Katie Hoffman <ktahoffman@gmail.com>
2014-07-12 20:29:42 -0400

    add intializer for oauth fb to .git

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_key
-end
